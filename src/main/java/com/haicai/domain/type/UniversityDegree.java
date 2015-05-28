85738071ed0e2feaad493345f5df373dd6f668ff
cainzhong <cainzhong@aliyun.com>
2015-05-28 23:14:38 +0800

    get enum using string

--- src/main/java/com/haicai/domain/type/UniversityDegree.java
+++ src/main/java/com/haicai/domain/type/UniversityDegree.java
@@ -3,21 +3,22 @@ package com.haicai.domain.type;
 import lombok.ToString;
 
 /**
- * Enum: UNIVERSITY_DEGREE - Bachelor("BACHELOR",0), Master("MASTER",1), Doctor("DOCTOR",2), PostDoctor("POST-DOCTOR",3), Other("OTHER",4)
+ * Enum: UNIVERSITY_DEGREE - Bachelor("BACHELOR",0), Master("MASTER",1),
+ * Doctor("DOCTOR",2), PostDoctor("POST-DOCTOR",3), Other("OTHER",4)
  *
  * @author Cain
  *
  */
 @ToString
 public enum UniversityDegree {
-	Bachelor("BACHELOR",0), Master("MASTER",1), Doctor("DOCTOR",2), PostDoctor("POST-DOCTOR",3), Other("OTHER",4);
+	Bachelor("T_UNIVERSITY_DEGREE_BACHELOR", 0), Master("T_UNIVERSITY_DEGREE_MASTER", 1), Doctor("T_UNIVERSITY_DEGREE_DOCTOR", 2), PostDoctor("T_UNIVERSITY_DEGREE_POSTDOCTOR", 3), Other("T_UNIVERSITY_DEGREE_OTHER", 4);
 
 	private String degree;
 	private int identify;
 
-	private UniversityDegree(String degree,int identify) {
+	private UniversityDegree(String degree, int identify) {
 		this.degree = degree;
-		this.identify=identify;
+		this.identify = identify;
 	}
 
 	public String getDegree() {
@@ -27,4 +28,23 @@ public enum UniversityDegree {
 	public int getIdentify() {
 		return this.identify;
 	}
+
+	// from String degree to Enum
+	public static UniversityDegree getUniversityDegree(String degree) {
+		switch (degree) {
+		case "T_UNIVERSITY_DEGREE_BACHELOR":
+			return Bachelor;
+		case "T_UNIVERSITY_DEGREE_MASTER":
+			return Master;
+		case "T_UNIVERSITY_DEGREE_DOCTOR":
+			return Doctor;
+		case "T_UNIVERSITY_DEGREE_POSTDOCTOR":
+			return PostDoctor;
+		case "T_UNIVERSITY_DEGREE_OTHER":
+			return Other;
+		default:
+			return Other;
+		}
+	}
+
 }
